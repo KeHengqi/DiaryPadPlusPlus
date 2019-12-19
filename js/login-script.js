792ff9b4f7c77111321b629cb6f9b78bbb629852
xtayex <75450574@qq.com>
2019-12-19 21:04:04 +0800

    login page update
    login page update

--- js/login-script.js
+++ js/login-script.js
@@ -3,3 +3,14 @@ $(function () {
         './img/login-bg.jpg'
     ])
 })
+
+$(document).ready(function () {
+    $('#register-link').click(function (e) { 
+        e.preventDefault();
+        $(location).attr('href', './main-page.html');
+    }); 
+    $('#lost-password-link').click(function (e) { 
+        e.preventDefault();
+        $(location).attr('href', 'http://www.baidu.com');
+    });
+});
